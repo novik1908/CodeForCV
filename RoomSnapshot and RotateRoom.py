3be0144d928f9122b3408a5f6ab9e0d5c55b661f
novik1908 <62353115+novik1908@users.noreply.github.com>
2020-06-21 18:48:23 +0300

    Update RoomSnapshot and RotateRoom.py

--- RoomSnapshot and RotateRoom.py
+++ RoomSnapshot and RotateRoom.py
@@ -25,22 +25,21 @@ class RadialPoint:
         return x, y
 
 class RoomSnapshot:
-    def __init__(self, strCsvData):
+    def __init__(self, csvfile):
         self.radialPoints = []
-        with open('05.csv', newline='') as csvfile:
+        with open(csvfile, newline='') as csvfile:
             spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
             for row in spamreader:
                 dist = float(row[0])
                 angle = float(row[1])
                 v = RadialPoint(angle, dist)
-                strCsvData.append(row)
                 self.radialPoints.append(v)
 
     def getXpoints(self):
         xpoints = []
         for i in range(len(self.radialPoints)):
             dist = self.radialPoints[i].dist
-            angle = self.radialPoints[i].angle
+            angle = self.radialPoints[i].angle + addAngle
             if dist < 0:
                 x = -dist * math.sin(math.radians(angle + 90))
             elif dist > 0:
@@ -54,7 +53,7 @@ class RoomSnapshot:
         ypoints = []
         for i in range(len(self.radialPoints)):
             dist = self.radialPoints[i].dist
-            angle = self.radialPoints[i].angle
+            angle = self.radialPoints[i].angle + addAngle
             if dist < 0:
                 y = -dist * math.sin(math.radians(angle + 180))
             elif dist > 0:
@@ -64,40 +63,14 @@ class RoomSnapshot:
             ypoints.append(y)
         return ypoints
 
-    def rotateRoom(appendAngle):
-        return appendAngle
+    def rotateRoom(self, addAngle):
+        self.angle = self.angle + addAngle
 
 
-
-def showOnConsoleWithRotation(appendAngle):
-    with open('05.csv', newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
-        for row in spamreader:
-            dist = float(row[0]) + float(RoomSnapshot.rotateRoom(appendAngle))
-            angle = float(row[1])
-            v = RadialPoint(angle, dist)
-            print('dist = {} angle = {} coords = {}'.format(dist, angle, v.getAsXY()))
-
-def showAsImageWithRotation(appendAngle):
-    x = []
-    y = []
-    with open('05.csv', newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
-        for row in spamreader:
-            dist = float(row[0]) + float(RoomSnapshot.rotateRoom(appendAngle))
-            angle = float(row[1])
-            v = RadialPoint(angle, dist)
-            x.append(v.getAsXY()[0])
-            y.append(v.getAsXY()[1])
-    plt.scatter(x, y, c='black', s=1)
+def showAsImageWithRotation(self):
+    plt.scatter([x for x in room.getXpoints()], [y for y in room.getYpoints()], c='black', s=1)
     plt.show()
 
-def selectHowToShowInfoWithRotation(typeOfDispl):
-    if typeOfDispl == "console":
-        return showOnConsoleWithRotation(appendAngle)
-    if typeOfDispl == "image":
-        return showAsImageWithRotation(appendAngle)
-
-appendAngle = input("Which angle append to rotate room? ")
-typeOfDispl = input("How to display info (console, image)? ")
-selectHowToShowInfoWithRotation(typeOfDispl)
+room = RoomSnapshot('05.csv')
+addAngle = -60
+showAsImageWithRotation(room)
